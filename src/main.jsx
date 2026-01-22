d8b93478a7e367342e0679fe1f264b50b0bf53ff
Huiseon Yi <heesun0514@gmail.com>
2026-01-22 16:35:51 +0000

    main.jsx - 기본 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>
 );
