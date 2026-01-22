11c942f578a459b7bee432e73fdf21a7dd2cde7e
Huiseon Yi <heesun0514@gmail.com>
2026-01-22 20:19:56 +0000

    Dashboard.jsx - 로그아웃 버튼 추가

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,43 @@
 import React from "react";
-import { Container,Typography } from "@mui/material";
+import { 
+Container,
+Typography,
+Button,
+Box
+} from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
 
 
 const Dashboard=()=>{
+  const navigate = useNavigate();
+  const username=localStorage.getItem('user');
+
+  const handleLoggout=()=>{
+    localStorage.removeItem('user');
+    navigate('/')
+    
+  };
+
+
+
   return (
     <Container>
-      <Typography variant="h4" sx={{mt:4}}>
+      <Box sx={{mt:4}}>
+      <Typography variant="h4">
         Dashboard
       </Typography>
-      <Typography variant="body1">
-        Welcome to the Dashboard
+      <Typography variant="body1" sx={{mb:3}}>
+        Welcome,{username}!
       </Typography>
+
+<Button
+variant="outlined"
+onClick={handleLoggout}
+>
+  Logout
+</Button>
+      </Box>
     </Container>
 
   );
