9278a287e38af3193b7a775207d20f61e2248eb4
Huiseon Yi <heesun0514@gmail.com>
2026-01-22 20:50:54 +0000

    components/Navbar.jsx - Material UI 적용

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { AppBar,Toolbar,Typography } from "@mui/material";
 
 const Navbar=()=>{
+    const username=localStorage.getItem('user') || 'User';
+
+
+{/*|| 'User'
+만약 localStorage에 값이 없으면 'User'로 기본값 사용
+즉, 사용자가 로그인하지 않았거나 정보가 없으면 기본적으로 "User"가 표시됩니다. */}
+
     return(
 
-        < div style={{
-            backgroundColor:'#1976d2',
-            color:'white',
-            padding:'16px',
-            display:'flex',
-            justifyContent :'space-between',
-            alignItems:'center'
-        }}>
-            <div>Advanced Todo App</div>
-            <div>User</div>
-        </div>
+   <AppBar postion="static">
+    <Toolbar>
+<Typography variant="h6" component="div" sx={{flexGrow:1}}>
+    Advanced Todo App
+</Typography>
+<Typography variant="body1">
+    {username}
+</Typography>
+    </Toolbar>
+   </AppBar>
     );
 };
 
