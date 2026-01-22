a4dfcf924e2349a462f75eb38f6f010ebf4614ee
Huiseon Yi <heesun0514@gmail.com>
2026-01-22 17:31:09 +0000

    Login.jsx - Step 2 (Material UI 도입)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Container, Typography } from '@mui/material';
+
+
+
+  {/* sm 화면이 아무리 커도 이 Container의 최대 너비는 약 600px까지만 허용한다 */}
+  {/* 텍스트 아래쪽에 기본 여백(margin-bottom)을 자동으로 추가*/}
 
 const Login=()=>{
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+  
+    <Container maxWidth="sm"> 
+<Typography variant="h4" gutterBottom>
+  Welcome
+</Typography>
+<Typography variant="body1" color="text.secondary">
+  Please endter your username to continue
+</Typography>
+    </Container>
+
   );
 };
 
