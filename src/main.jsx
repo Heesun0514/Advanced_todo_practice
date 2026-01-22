4a0dd7f26cfddff4ea8e2bb9231b016eb5741755
Huiseon Yi <heesun0514@gmail.com>
2026-01-22 16:49:45 +0000

    Step 2: 라우팅 구조 구축/main.jsx - BrowserRouter 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App.jsx';
+import {BrowserRouter} from 'react-router-dom'; // 추가
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter> {/* 추가 */}
     <App/>
+    </BrowserRouter> {/* 추가 */}
   </React.StrictMode>
 );
