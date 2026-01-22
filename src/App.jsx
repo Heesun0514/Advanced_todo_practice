9f1b30d4c8e2753eed289e6d35a93dc28ad6117c
Huiseon Yi <heesun0514@gmail.com>
2026-01-22 19:04:41 +0000

    App.jsx - 대시보드 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Routes,Route} from 'react-router-dom'; // 추가
 import Login from "./pages/Login"; // 추가
+import Dashboard from './pages/Dashboard';// 추가
 
 const App=()=>{
   return(
    <Routes>
     <Route path ="/" element={<Login/>}/>
+    <Route path ="/dashboard " element={<Dashboard/>}/> {/* 추가 */}
    </Routes>
   );
 };
