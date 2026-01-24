140986a3db5bf52a9e7107402c2a58577919201b
Huiseon Yi <heesun0514@gmail.com>
2026-01-24 22:46:06 +0000

    create firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8QuKv7z5t2wDZUWld4Ue5MqRf3guXZbs",
+  authDomain: "advtodo-9a486.firebaseapp.com",
+  projectId: "advtodo-9a486",
+  storageBucket: "advtodo-9a486.firebasestorage.app",
+  messagingSenderId: "577012438590",
+  appId: "1:577012438590:web:d7c3bd3d9916edbeb5236f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
